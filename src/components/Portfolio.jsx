529f3fcd6e5bf3cb6a40ebbed4b12b40c9d21d38
saswat <saswatranjan0602@gmail.com>
2023-07-22 12:52:39 +0530

    addding images

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import yelpCamp from "../assets/portfolio/yelpCamp.png"
+import food from '../assets/portfolio/food.png'
+import news from '../assets/portfolio/news.png'
+import note from '../assets/portfolio/note.png'
+import movie from '../assets/portfolio/movie.png'
+import tic from '../assets/portfolio/tictactoe.png'
+import todo from '../assets/portfolio/todo.png'
+
+
+
+const Portfolio = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Portfolio</p>
+          <p>Check my Work</p>
+
+        </div>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
