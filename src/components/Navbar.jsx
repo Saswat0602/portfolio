8bf21df4eeea0f46f40ff5fc65cc76f77ee5941e
saswat <saswatranjan0602@gmail.com>
2023-07-22 14:31:58 +0530

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import Navlinks from "./Navlinks";
 
 const Navbar = () => {
-    const[nav ,setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const links = [
     {
       id: 1,
@@ -27,46 +28,43 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center bg-black px-4  w-full h-20 text-white fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
       <div>
-        <h1 className="text-5xl font-signature  ml-2">Saswat</h1>
+        <h1 className="text-5xl font-signature ml-2">Yash</h1>
       </div>
+
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
-          <li
+          <Navlinks
             key={id}
-            className="px-4 capitalize hover:scale-105 duration-200 cursor-pointer text-gray-500 font-medium"
-          >
-            {link}
-          </li>
+            link={link}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          />
         ))}
       </ul>
+
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 text-gray-300 md:hidden "
+        className="cursor-pointer pr-4 md:hidden z-10 text-gray-500"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
       {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-gray-500">
-        {links.map(({ id, link }) => (
-          <li
-            key={id}
-            className="px-4 cursor-pointer capitalize py-6 text-4xl"
-          >
-            {link}
-          </li>
-      ))}
-      </ul>
-        
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <Navlinks
+              key={id}
+              link={link}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              onClick={() => setNav(!nav)}
+            />
+          ))}
+        </ul>
       )}
-
-
-
-    
     </div>
   );
 };
+
 
 export default Navbar;
