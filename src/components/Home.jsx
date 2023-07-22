782b5250c0a293b85ba17f89cb67a2ac927fcdd0
saswat <saswatranjan0602@gmail.com>
2023-07-22 16:56:00 +0530

    complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,32 +8,38 @@ const Home = () => {
     <>
       <div
         name="home"
-        className="w-full h-screen bg-gradient-to-b via-black from-black to-gray-800  "
+        className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
       >
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
           <div className="flex flex-col justify-center h-full">
             <h2 className="text-4xl sm:text-7xl font-bold text-white">
-              I'm a Mern stack Developer
+              I'm a Full Stack Developer
             </h2>
             <p className="text-gray-500 py-4 max-w-md">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde
               deserunt impedit atque est delectus? At dolorum nemo beatae eos
               repellendus atque harum, fuga v
             </p>
-            <div className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to to-blue-500 cursor-pointer">
-              <button>
+            <div>
+              <Link
+                to="portfolio"
+                smooth
+                duration={500}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              >
+                {" "}
                 Portfolio
-                <span>
-                  <HiArrowNarrowRight />
+                <span className="group-hover:rotate-90 duration-300">
+                  <HiArrowNarrowRight className="ml-1" />
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
           <div>
             <img
               src={hero}
               alt="my profile"
-              className="rounded-2xl mx-auto w-2/3 md:w-full"
+              className="rounded-2xl mx-auto w-2/3 md:w-full hero"
             />
           </div>
         </div>
